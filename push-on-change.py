d067a0b8c50349e100eebe8831255fde46d2cabc
Lucas Åström <lucasas@kth.se>
2014-11-15 22:10:16 +0100

    added first version of script

---
+++ push-on-change.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys
+import time
+import subprocess
+import httplib
+import urllib
+
+from token import *
+
+def notify(title, message):
+	post = {
+		"token": app_token,
+		"user": user_token,
+		"message": message,
+		"title": title
+	}
+
+	conn = httplib.HTTPSConnection("api.pushover.net:443")
+	conn.request("POST", "/1/messages.json",
+		urllib.urlencode(post),
+		{ "Content-type": "application/x-www-form-urlencoded" })
+	conn.getresponse()
+
+command = sys.argv[1]
+delay = float(sys.argv[2])
+title = sys.argv[3]
+
+lastOutput = ""
+while True:
+	output = subprocess.Popen(command, stdout=subprocess.PIPE).communicate()[0]
+	if lastOutput != output:
+		notify(title, output)
+		lastOutput = output
+	time.sleep(delay)
